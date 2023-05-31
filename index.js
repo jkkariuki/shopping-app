eaf18e712aded68b9f0e76b9491ee56777fd7a8e
jkkariuki <jkkariuki15@gmail.com>
2023-05-30 23:59:09 -0400

    checkout issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 const app = express();
-app.use(express.static(path.join(__dirname + "/client/build")));
+app.use(express.static(path.join(__dirname + "client/build")));
 app.use(cors());
 app.use(express.json());
 
