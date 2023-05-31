8cc4b28c98449266927d301f99d35bd738f5b19b
jkkariuki <jkkariuki15@gmail.com>
2023-05-30 20:25:09 -0400

    fix port error

--- index.js
+++ index.js
@@ -43,13 +43,4 @@ app.post("/checkout", async (req, res) => {
   );
 });
 
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 app.listen(PORT, () => console.log("listening on port 5000"));
