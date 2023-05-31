72b8d2f917055c4f888088f7048c803a2ec591ca
jkkariuki <jkkariuki15@gmail.com>
2023-05-30 20:11:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+const cors = require("cors");
+const PORT = process.env.port || 5000;
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const app = express();
+app.use(express.static(path.join(__dirname + "/public")));
+app.use(cors());
+app.use(express.json());
+
+app.post("/checkout", async (req, res) => {
+  console.log(req.body);
+  const items = req.body.items;
+  console.log(items);
+  let lineItems = [];
+  items.map((item) => {
+    lineItems.push({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.title,
+          images: [item.thumbnail],
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.qty,
+    });
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url:
+      "http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}",
+    cancel_url: "http://localhost:3000/cancel",
+  });
+
+  res.send(
+    JSON.stringify({
+      url: session.url,
+    })
+  );
+});
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log("listening on port 5000"));
